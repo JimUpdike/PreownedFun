cbbb88a481da03ffc2ca21bb5354ec317b4cbc9d
Jim Updike <Jim.Updike445@gmail.com>
2012-02-20 12:49:21 -0500

    Adding basic html template (I hope)

---
+++ index.php
@@ -0,0 +1,29 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<link rel="stylesheet" href="style.css" type="text/css" />
+<body>
+	<div id="login">
+    <ul class=loginUL>
+	<li class = loginLI>
+	Login or account settings should go here.
+	</li>
+	</ul>
+	</div>
+	<div id="banner">
+	<form action=searchController.php method=get>
+     <input type=text name=username />
+    <input type=submit value="Search!" />
+	<br/>	
+	
+	</div>
+    <div id="left">
+		I was thinking possible like a navigation bar here, like the ones you see on ebay/amazon
+	</div>
+
+    <div id="right">
+         And here is where the main display will be
+		</div>
+
+</body>
+
+</html>
