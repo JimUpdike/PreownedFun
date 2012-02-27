0e3e1a3a0243ce49de678291db41073c35ba3a00
Jim Updike <Jim.Updike445@gmail.com>
2012-02-27 12:51:12 -0500

    I guess I need to add the files everytime I want to change them

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
-
+<?php /*
 <link rel="stylesheet" href="style.css" type="text/css" />
 <body>
 	<div id="login">
@@ -37,8 +37,8 @@
 		 </li>
 		
 		 <li class=browseLI>
-			Poor
 			</li>
+			Poor
 			<li class=browseLI>
 			Mediocre
 		   </li>
@@ -136,5 +136,126 @@
 		</div>
 
 </body>
+*/?>
 
+<?php include "template.php"; ?>
+
+<div id="left">
+<i><b><u>Browse Box</u></b></i>
+		<ul class=browseUL>
+		<li class=browseLIHeader>
+			Price:
+		 </li>
+		 <li class=browseLI>
+			0.1-10.00$
+			</li>
+			<li>
+			10.01-20.00$
+			</li>
+			<li>
+			etc
+		   </li>
+		    <br/>
+			<li class=browseLIHeader>
+			Quality
+		 </li>
+		
+		 <li class=browseLI>
+			</li>
+			Poor
+			<li class=browseLI>
+			Mediocre
+		   </li>
+			<li class=browseLI>
+			Fair
+			</li>
+			<li class=browseLI>
+			Good
+		   </li>
+		   <li class=browseLI>
+			Great
+		   </li>
+		   <li class=browseLI>
+			Mint
+		   </li>
+		</ul>
+	
+
+</div>
+<div id="right">
+       <table class="displayTable">
+	   <tr>
+	   <th> Name </th>
+	   <th> Quality </th>
+	   <th> Price </th>
+	   <th> Date Posted </th>
+	   </tr>
+	   <tr>
+	   <td> test The Game </td>
+	   <td> Poor </td>
+	   <td> 1000000.00$ </td>
+	   <td> 2/22/2012 </td>
+		</tr>	 
+		<tr>
+	   	   <td class=off> Perfect The Game </td>
+	   <td class=off> Mint </td>
+	   <td class=off> .01$ </td>
+	   <td class=off> 2/12/2012 </td>
+	   </tr>
+	   <tr>
+	   <td> test The Game </td>
+	   <td> Poor </td>
+	   <td> 1000000.00$ </td>
+	   <td> 2/22/2012 </td>
+		</tr>
+			<tr>
+	   	   <td class=off> Perfect The Game </td>
+	   <td class=off> Mint </td>
+	   <td class=off> .01$ </td>
+	   <td class=off> 2/12/2012 </td>
+	   </tr>
+	   <tr>
+	   <td> test The Game </td>
+	   <td> Poor </td>
+	   <td> 1000000.00$ </td>
+	   <td> 2/22/2012 </td>
+		</tr>
+			<tr>
+	   	   <td class=off> Perfect The Game </td>
+	   <td class=off> Mint </td>
+	   <td class=off> .01$ </td>
+	   <td class=off> 2/12/2012 </td>
+	   </tr>
+	   <tr>
+	   <td> test The Game </td>
+	   <td> Poor </td>
+	   <td> 1000000.00$ </td>
+	   <td> 2/22/2012 </td>
+		</tr>
+			<tr>
+	   	   <td class=off> Perfect The Game </td>
+	   <td class=off> Mint </td>
+	   <td class=off> .01$ </td>
+	   <td class=off> 2/12/2012 </td>
+	   </tr>
+	   <tr>
+	   <td> test The Game </td>
+	   <td> Poor </td>
+	   <td> 1000000.00$ </td>
+	   <td> 2/22/2012 </td>
+		</tr>
+			<tr>
+	   	   <td class=off> Perfect The Game </td>
+	   <td class=off> Mint </td>
+	   <td class=off> .01$ </td>
+	   <td class=off> 2/12/2012 </td>
+	   </tr>
+	   <tr>
+	   <td> test The Game </td>
+	   <td> Poor </td>
+	   <td> 1000000.00$ </td>
+	   <td> 2/22/2012 </td>
+		</tr>
+	   </table>
+</div>
 </html>
