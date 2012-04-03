50136c7e16bac60f1d22ea0ad51828a8e7ae89a7
Alex Gilley <Alex@Alex-PC.(none)>
2012-04-02 20:11:06 -0400

    Fixed user index.

--- userList.php
+++ userList.php
@@ -17,17 +17,29 @@ include "template.php"; ?>
 <div id="right">
 <h2>User Index<h2></br>
 	<table class="displayTable">
-	<tr><th>Username</th><th>Rating</th><tr>
+	<tr><th>Username</th><th>Email</th><th>Rating</th><tr>
 	<?php
 	include('db_connect.php');
-	$query = "SELECT * FROM userRep ORDER BY username";
+	$query = "SELECT * FROM users ORDER BY username";
 	$result = mysqli_query($db, $query) or die("Error Querying Database");
-	while($row = mysqli_fetch_array($result)) {
+	$i = 0;	
+	while($row = mysqli_fetch_array($result)){	
 		$username = $row['username'];
 		$rating = $row['rating'];
-		echo "<tr><td><a href='profile.php?username=$username'>$username<a></td><td>$rating</td></tr>\n";
+		if ($rating == -1.00){
+			$rating = 'This user has not been rated.';
+		}
+		$email = $row['email'];
+		if ($i % 2 == 0){
+			$td = "<td class=off>";
+		}
+		else {
+			$td = "<td>";
+		}
+		echo"<tr>$td <a href='profile.php?username=$username'>$username<a></td> $td $email </td> $td rating </tr>"; 
+		$i++;
 	}
-					
+			
 	mysqli_close($db);
 	?>
 	</table>
